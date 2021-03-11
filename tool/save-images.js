e3e63b9b8133b019c4dc89712c17836b1ad7a889
neoarc <neoarcturus@gmail.com>
2021-03-11 16:14:29 +0900

    Update save-images.js

--- tool/save-images.js
+++ tool/save-images.js
@@ -1,119 +1,102 @@
-//#!/usr/bin/env bash
-//# github에 올린 user-images를 자동으로 다운로드합니다.
-var shell = require('shelljs');
-var fs = require('fs');
+// 2021-03-10 neoarc; 기존의 save-images.sh를 node.js 스크립트로 포팅한 것
+//                    비교하기 용이하도록 변수명은 기존값을 유지했음 (대문자 변수들)
+//                    sed, ag 등의 명령이 사용 불가능할 경우 사용하자.
 
-//NUM=1855714
-let NUM = 963958; // 각 계정별 고유값으로 추정됨
+// github에 올린 user-images를 자동으로 다운로드 합니다.
+const shell = require('shelljs');
+const fs = require('fs');
 
-//CHANGE_LIST=`git diff --exit-code --cached --name-only --diff-filter=ACM -- "*.md"`
-//var ret = shell.exec(`git diff --exit-code --cached --name-only --diff-filter=ACM -- "*.md"`);
+// 각 계정별 고유값으로 추정됨
+// 본인의 github.io Repo Issue 페이지에 글을 작성할 때 확인 가능
+let NUM = 963958; 
+
+// 변경된 파일 목록을 얻어온다.
 var ret = shell.exec(`git diff --exit-code --name-only -- "*.md"`);
 var CHANGE_LIST = ret.stdout.split('\n');
 console.log(`CHANGE_LIST = ${CHANGE_LIST}`);
 
 var SUCCESS_COUNT=0
 var FAIL_COUNT=0
-//for CHANGED_FILE in $CHANGE_LIST; do
 CHANGE_LIST.forEach((CHANGED_FILE) => {
-    //echo $CHANGED_FILE
     if (!CHANGED_FILE || CHANGED_FILE.length == 0) return;
-
 	console.log(`CHANGED_FILE = ${CHANGED_FILE}`);
-	var ex = /_(.*).md$/
 
-    // 주의: _wiki 경로로 그대로 복사하면 Jekyll 이 복사하지 않는다.
+    // 주의: _wiki 경로로 그대로 복사하면 Jekyll이 _site 폴더로 복사하지 않는다.
     //       _ 로 시작하는 폴더는 자동으로 제외되는 듯 하다.
-    // _wiki/sfmb_betatest_how_to_make_parallax_background.md
-    //  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    let dirMatchResult = CHANGED_FILE.match(ex);
-    //console.debug(`dirMatchResult = ${dirMatchResult}`);
+    //
+    // _wiki/aaa/bbb/ccc.md
+    //  ^^^^^^^^^^^^^^^^
+    let dirMatched = CHANGED_FILE.match(/_(.*).md$/);
+    if (!dirMatched) return;
 
-    if (!dirMatchResult) return;
-    var DIR_NAME = dirMatchResult[1].split('/').join('\\');
-    //DIR_NAME=`echo $CHANGED_FILE | sed -E 's,^.+/([^\.]+)\.md$,\1,'`
+    var DIR_NAME = dirMatched[1].split('/').join('\\');
     console.log(`DIR_NAME = ${DIR_NAME}`);
+
+    // .md를 제외한 이름으로 계층구조의 폴더 생성
     shell.exec(`mkdir .\\post-img\\${DIR_NAME}`)
 	
-    // CHANGED_FILE을 열어서 아래 조건으로 검색
+    // CHANGED_FILE을 열어서, githubuser* 에 링크된 이미지 파일을 찾는다.
     var URI_LIST = [];
     {
-        let buf = fs.readFileSync(`${CHANGED_FILE}`);
-        let lines = buf.toString().split('\n');
-        lines.forEach((line) => {
-            let ex2 = /https\:\/\/user-images\.githubuser.*?\/963958\/.*?(png|jpg|gif)/
-            let ex2result = line.match(ex2);
-            if (!ex2result) return;
-            //console.debug(`ex2result = ${ex2result[0]}`);
-            URI_LIST.push(ex2result[0])
+        fs.readFileSync(`${CHANGED_FILE}`)
+          .toString()
+          .split('\n')
+          .forEach((line) => {
+            let imageUriMatched = line.match(/https\:\/\/user-images\.githubuser.*?\/963958\/.*?(png|jpg|gif)/);
+            if (!imageUriMatched) return;
+
+            URI_LIST.push(imageUriMatched[0]);
         });
     }
-	//var ret = shell.exec(`ag "https://user-images\.githubuser.*?\/$NUM\/.*?(png|jpg|gif)" -o $CHANGED_FILE`);
-    //var URI_LIST = ret.stdout.split('\n');
 
-	//for URI in $URI_LIST; do
+    // 찾아낸 URI에 대하여 처리한다.
 	URI_LIST.forEach((URI) => {    
-        //var FILE_NAME = `echo $URI | sed 's,^.*/,,'`;
-        let ex3 = /\/([a-zA-Z_\-0-9]+.(png|jpg|gif))$/
-        let ex3result = URI.match(ex3);
-        //console.debug(`ex3result = ${ex3result}`);
-        console.log(`TARGET: ${URI}`);
-		
-        if (!ex3result) return;
-        let FILE_NAME = ex3result[1];
+        // 파일이름만 잘라낸다.
+        let imageFilenameMatched = URI.match(/\/([a-zA-Z_\-0-9]+.(png|jpg|gif))$/);
+        if (!imageFilenameMatched) return;
+
+        console.log(`TARGET: ${URI}`);		
+        let FILE_NAME = imageFilenameMatched[1];
+
+        // 이미지 파일을 github에서 로컬경로로(post-img/...) 다운로드한다.
         let downloadResult = shell.exec(`curl -s ${URI} > ./post-img/${DIR_NAME}/${FILE_NAME}`);
-        
-        //if [ "$?" == "0" ]; then
-        //console.debug(`downloadResult.code = ${downloadResult.code}`);
         if (downloadResult.code == 0) {
-            //echo "DOWNLOAD SUCCESS: $FILE_NAME"
-            console.log(`DOWNLOAD SUCCESS: ${FILE_NAME}`)
-            //sed -i '' -E 's, *https://.*('"$FILE_NAME"') *, /post-img/'$DIR_NAME'/\1 ,g' $CHANGED_FILE
+            console.log(`DOWNLOAD SUCCESS: ${FILE_NAME}`);
+
+            // 다운로드에 성공했으면, 위키 문서를 열어 모든 URI를 로컬경로로 replace한다.
             {
-                // #TODO  
-                let buf = fs.readFileSync(`${CHANGED_FILE}`);
-                let lines = buf.toString().split('\n');
-                let lines2 = [];
-                lines.forEach((line) => {
-                    //let ex4 = /https\:\/\/.*('`${FILE_NAME}`')/
-                    //if (!ex4.test(line)) {
-                    if (line.indexOf(URI) < 0) {
-                        lines2.push(line);
-                        //console.debug(`NO MATCH: ${line}`)
-                    } else {
-                        console.debug(`    FROM: ${line}`)
+                let newLines = [];
+                fs.readFileSync(`${CHANGED_FILE}`)
+                  .toString()
+                  .split('\n')
+                  .forEach((line) => {
+                    if (line.indexOf(URI) < 0) 
+                        newLines.push(line);
+                    else {
                         let DIR_NAME_AS_URI = DIR_NAME.split('\\').join('/');
-                        let line2 = line.replace(URI, `/post-img/${DIR_NAME}/${FILE_NAME}`);
-                        console.debug(`      TO: ${line2}`)
-                        lines2.push(line2);
+                        let convertedLine = line.replace(URI, `/post-img/${DIR_NAME_AS_URI}/${FILE_NAME}`);
+                        newLines.push(convertedLine);
                     }
                 });
-                fs.writeFileSync(`${CHANGED_FILE}`, lines2.join('\n'))
+                fs.writeFileSync(`${CHANGED_FILE}`, newLines.join('\n'));
             }
 
-            // #TODO
-            //git add post-img/$DIR_NAME/$FILE_NAME
+            // 잘 변환한 이미지 파일을 커밋할 수 있도록 add 한다.
             shell.exec(`git add post-img/${DIR_NAME}/${FILE_NAME}`)
-
-            //SUCCESS_COUNT=$((SUCCESS_COUNT+1))
             ++SUCCESS_COUNT
         } else {
-            //echo "DOWNLOAD FAIL: $FILE_NAME"
             console.log(`DOWNLOAD FAIL: ${FILE_NAME}`)
             
-            //rm -f ./post-img/$DIR_NAME/$FILE_NAME
+            // 다운로드에 실패했다면, 받다 만 찌꺼기 파일을 삭제한다.
             shell.exec(`del ./post-img/${DIR_NAME}/${FILE_NAME}`)
-
-            //FAIL_COUNT=$((FAIL_COUNT+1))
             ++FAIL_COUNT
         }
     });
 
-	//git add $CHANGED_FILE
+	// 해당 md 파일을 다시 add 한다.
     shell.exec(`git add ${CHANGED_FILE}`)
 });
 
-//done
-
-//printf "Success: %d, Fail: %d\n" $SUCCESS_COUNT $FAIL_COUNT
 console.log(`Success: ${SUCCESS_COUNT}, Fail: ${FAIL_COUNT}`);
+
+// 끝
